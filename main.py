0449910852d15b1e1ad53b9a342a62670e411241
James Pretorius <jamesmpretorius@gmail.com>
2022-01-15 22:27:25 -0500

    pb as default relay

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def main():
     asyncio.set_event_loop_policy(asyncio_glib.GLibEventLoopPolicy())
 
     config_manager = ConfigManager(args.config)
-    # remote_relay = AsyncPushbulletRelay(config_manager.get_api_key())
-    remote_relay = TestNotificationRelay()
+    remote_relay = AsyncPushbulletRelay(config_manager.get_api_key())
+    # remote_relay = TestNotificationRelay()
 
     local_notification_manager = LocalNotificationManager(config_manager)
     remote_relay.add_listener(local_notification_manager)
